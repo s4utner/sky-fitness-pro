1fa2ad6c244fa365cc3cb8436f372f6053f6cf82
Heropaster <heropasta@mail.ru>
2024-02-13 21:34:10 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => (
+  <div>
+    <h1>Test</h1>
+  </div>
+)
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), viteTsConfigPaths()],
+});
