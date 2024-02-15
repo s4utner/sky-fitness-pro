c9d307c09c88abad8b3195da40712156b8c1e3ed
Greblips <Evgenspilbegrg@gmail.com>
2024-02-15 21:01:33 +0300

    feat(lang) : Добавлен шрифт StratosSkyeng

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,8 +2,8 @@ import { Title } from 'components/UI/Title/Title.tsx'
 import { Button } from 'components/UI/Button/Button.tsx'
 
 export const Main = () => (
-    <div>
-      <Title color={'red'}>Заголовок</Title>
-      <Button>Нажми</Button>
-    </div>
-  )
+  <div>
+    <Title color={'red'}>Заголовок</Title>
+    <Button>Нажми</Button>
+  </div>
+)
