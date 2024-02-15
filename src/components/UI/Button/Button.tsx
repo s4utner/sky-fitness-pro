9f85fed027c63a3376a6444a84bd6c3b575446f2
Greblips <Evgenspilbegrg@gmail.com>
2024-02-15 22:53:27 +0300

    <feat> добавлена кнопка в комопненты

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,4 +1,17 @@
 import styles from './Button.module.scss'
-import type { FC, PropsWithChildren } from 'react'
+import type { FC, ButtonHTMLAttributes } from 'react'
 
-export const Button: FC<PropsWithChildren> = ({ children }) => <button className={styles.button}>{children}</button>
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+  variant?: string
+  square?: boolean
+  disabled?: boolean
+  title?: string
+  onClick: () => void
+}
+
+export const ButtonUI: FC<ButtonProps> = ({ title, disabled = false, onClick }) => (
+  <button disabled={disabled} type="button" className={styles.btn} onClick={onClick}>
+    {title}
+  </button>
+)
