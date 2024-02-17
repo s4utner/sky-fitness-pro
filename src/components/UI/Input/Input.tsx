499234f1aaa7fcc00e3540d91508356e67a83bd2
Denis <den.sautner@mail.ru>
2024-02-17 12:57:54 +0600

    added value attribute

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -3,8 +3,9 @@ import type { FC, InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholderText: string
+  value: string
 }
 
-export const Input: FC<InputProps> = ({ placeholderText }) => {
-  return <input className={styles.input} type="text" placeholder={placeholderText} />
+export const Input: FC<InputProps> = ({ placeholderText, value }) => {
+  return <input className={styles.input} type="text" placeholder={placeholderText} value={value} />
 }
