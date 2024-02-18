cc74ecf1ce0aa587e7498b7361de09ef8050cc7a
Alex Volo <alex_volo@bk.ru>
2024-02-18 23:26:17 +0900

    feat: add Main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,14 +1,46 @@
-import { Title } from 'components/UI/Title/Title.tsx'
-import { Button } from 'components/UI/Button/Button.tsx'
+import { Header, Button, FitnessCard } from 'components'
+import sticker from './img/sticker.png'
+import yogaImg from './img/yoga.png'
+import stratchingImg from './img/stratching.png'
+import danceImg from './img/dance.png'
+import stepImg from './img/step.png'
+import bodyflexImg from './img/bodyflex.png'
+
 import style from './Main.module.scss'
 
-const handleclick = () => console.log('Hello')
+export const Main = () => {
+  const fakeState = [
+    { title: 'Йога', img: yogaImg },
+    { title: 'Стретчинг', img: stratchingImg },
+    { title: 'Танцевальный фитнес', img: danceImg },
+    { title: 'Степ-аэробика', img: stepImg },
+    { title: 'Бодифлекс', img: bodyflexImg },
+  ]
+  const cards = fakeState.map((card) => (
+    <FitnessCard key={card.title} image={card.img}>
+      {card.title}
+    </FitnessCard>
+  ))
 
-export const Main = () => (
-  <div className={style.container}>
-    <Title color="red">Заголовок</Title>
-    <Button variant="green" onClick={handleclick}>
-      навание кнопки
-    </Button>
-  </div>
-)
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <Header color="white" />
+        <h1 className={style.topTitle}>Онлайн-тренировки для занятий дома</h1>
+
+        <div className={style.leadGroup}>
+          <p className={style.leadText}>Начните заниматься спортом и улучшите качество жизни</p>
+          <img className={style.sticker} src={sticker} alt="Измени свое тело за полгода" />
+        </div>
+
+        <div className={style.fitnessCards}>{cards}</div>
+
+        <footer className={style.footer}>
+          <Button onClick={() => window.scrollTo(0, 0)} variant="green" width={150}>
+            Наверх ↑
+          </Button>
+        </footer>
+      </div>
+    </div>
+  )
+}
