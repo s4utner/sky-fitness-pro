ebaef07732d2ea68300c0e0b3ab14f815185ed9c
Alex Volo <alex_volo@bk.ru>
2024-02-18 23:04:22 +0900

    feat: add FitnessCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Logo } from 'components/UI/Logo/Logo'
 export { Button } from 'components/UI/Button/Button'
 export { Header } from 'components/Header/Header'
+export { FitnessCard } from 'components/FitnessCard/FitnessCard'
