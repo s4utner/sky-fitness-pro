735597b472e04779eb153426f9b391eafb17fb86
Alex-Volo <122879924+Alex-Volo@users.noreply.github.com>
2024-02-26 03:40:02 +0900

    Api (#13)
    * feat: add API

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,4 +1,11 @@
 import type { FC, PropsWithChildren } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-export const Providers: FC<PropsWithChildren> = ({ children }) => <BrowserRouter>{children}</BrowserRouter>
+const client = new QueryClient()
+
+export const Providers: FC<PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={client}>
+    <BrowserRouter>{children}</BrowserRouter>
+  </QueryClientProvider>
+)
