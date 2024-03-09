7b93266dbac987c5119b021b79732a6c92b3d801
Greblips <Evgenspilbegrg@gmail.com>
2024-03-09 19:38:47 +0300

    feat: вынес логику обработки инпута в отдельную функию

--- src/helpers/helpersFunction.ts
+++ src/helpers/helpersFunction.ts
@@ -4,3 +4,29 @@ export const validateEmail = (login: string): boolean => {
   const emailRegex = /^[\w%+.-]+@[\d.A-Za-z-]+\.[a-z]{2,}$/
   return login.length >= 6 && login.length <= 64 && emailRegex.test(login)
 }
+
+export const validateInput = (login: string, password: string, isLoginMode: boolean, repeatPassword?: string) => {
+  switch (true) {
+    case !login && !password: {
+      return 'Все поля должны быть заполнены'
+    }
+    case !login: {
+      return 'Введите логин'
+    }
+    case !password: {
+      return 'Введите пароль'
+    }
+    case !validateEmail(login): {
+      return 'Введите корректный email'
+    }
+    case !validatePassword(password): {
+      return 'Пароль должен содержать от 6 до 64 символов'
+    }
+    case !isLoginMode && password !== repeatPassword: {
+      return 'Пароли должны совпадать!'
+    }
+    default: {
+      return null
+    }
+  }
+}
--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useStore } from 'store/AuthStore'
 import { createNewUser, loginUser } from 'services/api'
-import { validateEmail, validatePassword } from 'helpers/helpersFunction'
+import { validateInput } from 'helpers/helpersFunction'
 import styles from './AuthPage.module.scss'
 
 export function AuthPage() {
@@ -26,86 +26,128 @@ export function AuthPage() {
   const handleIsLoginMode = () => {
     setIsLoginMode(false)
   }
+
   // Функция входа пользователя и валидация
   const handleLogin = async (e: { preventDefault: () => void }) => {
     e.preventDefault()
-    switch (true) {
-      case !login && !password: {
-        setErrorMessage('Все поля должны быть заполнены')
-        return
-      }
-      case !login: {
-        setErrorMessage('Введите логин')
-        return
-      }
-      case !password: {
-        setErrorMessage('Введите пароль')
-        return
-      }
-      case !validateEmail(login as string): {
-        setErrorMessage('Введите корректный email')
-        return
-      }
-      case !validatePassword(password as string): {
-        setErrorMessage('Пароль должен содержать от 6 до 64 символов')
-        return
-      }
-      default: {
-        try {
-          const response = await loginUser({ email: login as string, password: password as string })
+    const errorMessage = validateInput(login as string, password as string, true)
 
-          setUser(response)
-          navigate('/profile')
-          return response
-        } catch (error) {
-          if (error instanceof Error) setErrorMessage(error.message)
-        }
-      }
+    if (errorMessage) {
+      setErrorMessage(errorMessage)
+      return
+    }
+
+    try {
+      const response = await loginUser({ email: login as string, password: password as string })
+      setUser(response)
+      navigate('/profile')
+      return response
+    } catch (error) {
+      if (error instanceof Error) setErrorMessage(error.message)
     }
   }
 
   // Функция регистрации пользователя и валидация
   const handleRegistration = async (e: { preventDefault: () => void }) => {
     e.preventDefault()
-    switch (true) {
-      case !login && !password: {
-        setErrorMessage('Все поля должны быть заполнены')
-        break
-      }
-      case !login: {
-        setErrorMessage('Введите логин')
-        break
-      }
-      case !password: {
-        setErrorMessage('Введите пароль')
-        break
-      }
-      case password !== repeatPassword: {
-        setErrorMessage('пароли должны совпадать!')
-        break
-      }
-      case !validateEmail(login as string): {
-        setErrorMessage('Введите корректный email')
-        return
-      }
-      case !validatePassword(password as string): {
-        setErrorMessage('Пароль должен содержать от 6 до 64 символов')
-        return
-      }
-      default: {
-        try {
-          const response = await createNewUser({ email: login as string, password: password as string })
-          console.log(response, 'Это ответ на логин')
+    const errorMessage = validateInput(login as string, password as string, false, repeatPassword as string)
 
-          setUser(response)
-          navigate('/profile')
-          return response
-        } catch (error) {
-          if (error instanceof Error) setErrorMessage(error.message)
-        }
-      }
+    if (errorMessage) {
+      setErrorMessage(errorMessage)
+      return
+    }
+
+    try {
+      const response = await createNewUser({ email: login as string, password: password as string })
+      setUser(response)
+      navigate('/profile')
+      return response
+    } catch (error) {
+      if (error instanceof Error) setErrorMessage(error.message)
     }
   }
+
+  // Функция входа пользователя и валидация
+  // const handleLogin = async (e: { preventDefault: () => void }) => {
+  //   e.preventDefault()
+  //   switch (true) {
+  //     case !login && !password: {
+  //       setErrorMessage('Все поля должны быть заполнены')
+  //       return
+  //     }
+  //     case !login: {
+  //       setErrorMessage('Введите логин')
+  //       return
+  //     }
+  //     case !password: {
+  //       setErrorMessage('Введите пароль')
+  //       return
+  //     }
+  //     case !validateEmail(login as string): {
+  //       setErrorMessage('Введите корректный email')
+  //       return
+  //     }
+  //     case !validatePassword(password as string): {
+  //       setErrorMessage('Пароль должен содержать от 6 до 64 символов')
+  //       return
+  //     }
+  //     default: {
+  //       try {
+  //         const response = await loginUser({ email: login as string, password: password as string })
+
+  //         setUser(response)
+  //         navigate('/profile')
+  //         return response
+  //       } catch (error) {
+  //         if (error instanceof Error) setErrorMessage(error.message)
+  //       }
+  //     }
+  //   }
+  // }
+
+  // // Функция регистрации пользователя и валидация
+  // const handleRegistration = async (e: { preventDefault: () => void }) => {
+  //   e.preventDefault()
+  //   switch (true) {
+  //     case !login && !password: {
+  //       setErrorMessage('Все поля должны быть заполнены')
+  //       break
+  //     }
+  //     case !login: {
+  //       setErrorMessage('Введите логин')
+  //       break
+  //     }
+  //     case !password: {
+  //       setErrorMessage('Введите пароль')
+  //       break
+  //     }
+  //     case password !== repeatPassword: {
+  //       setErrorMessage('пароли должны совпадать!')
+  //       break
+  //     }
+  //     case !validateEmail(login as string): {
+  //       setErrorMessage('Введите корректный email')
+  //       return
+  //     }
+  //     case !validatePassword(password as string): {
+  //       setErrorMessage('Пароль должен содержать от 6 до 64 символов')
+  //       return
+  //     }
+  //     default: {
+  //       try {
+  //         const response = await createNewUser({ email: login as string, password: password as string })
+  //         console.log(response, 'Это ответ на логин')
+
+  //         setUser(response)
+  //         navigate('/profile')
+  //         return response
+  //       } catch (error) {
+  //         if (error instanceof Error) setErrorMessage(error.message)
+  //       }
+  //     }
+  //   }
+  // }
+
   // Сбрасывает ошибку при вводе в инпут и при переключении на регистрацию
   useEffect(() => {
     setErrorMessage('')
